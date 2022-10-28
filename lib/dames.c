562452cb1b893cf04102712d1f16e63317e7177e
Sours <Sours>
2022-10-28 13:32:41 +0200

    Première version

--- lib/dames.c
+++ lib/dames.c
@@ -2,6 +2,17 @@
 
 #include <stdio.h>
 
+void game() {
+  pion board[WIDTH_TABLE][HEIGHT_TABLE];
+
+  init_table(board);
+  while (true) {
+    print_board(board);
+    player_turn(board, PLAYER_WHITE);
+    print_board(board);
+    player_turn(board, PLAYER_BLACK);
+  }
+}
 void init_table(pion grille[][WIDTH_TABLE]) {
   for (int y = 0; y < HEIGHT_TABLE; y++) {
     for (int x = 0; x < WIDTH_TABLE; x++) {
@@ -17,7 +28,13 @@ void init_table(pion grille[][WIDTH_TABLE]) {
 }
 
 void print_board(pion grille[][WIDTH_TABLE]) {
+  printf("   ");
+  for (int i = 0; i < WIDTH_TABLE; i++) {
+    printf("|%d|", i);
+  }
+  printf("\n");
   for (int y = 0; y < HEIGHT_TABLE; y++) {
+    printf("|%d|", y);
     for (int x = 0; x < WIDTH_TABLE; x++) {
       printf("|");
       if (grille[y][x] == PION_BLACK) {
@@ -35,59 +52,62 @@ void print_board(pion grille[][WIDTH_TABLE]) {
 void print_moves(pion grille[][WIDTH_TABLE], player current_player);
 void print_combos(pion grille[][WIDTH_TABLE], player current_player);
 
-void ask_joueur(pion grille[][WIDTH_TABLE], pos piece, pos dest,
-                player current_player) {
-  selection_pion(grille, piece, current_player);
+void ask_joueur_move(pion grille[][WIDTH_TABLE], move* his_move,
+                     move possible_moves[], int nb_moves) {
+  printf("%d coups sont possibles\n", nb_moves);
+  for (int i = 0; i < nb_moves; i++) {
+    printf("x = %d y = %d peut aller en x = %d y = %d\n",
+           possible_moves[i].piece.x, possible_moves[i].piece.y,
+           possible_moves[i].destination.x, possible_moves[i].destination.y);
+  }
+  selection_pion(grille, &(his_move->piece), possible_moves, nb_moves);
+  selection_move(grille, his_move->piece, &(his_move->destination),
+                 possible_moves, nb_moves);
 }
 
-void selection_pion(pion grille[][WIDTH_TABLE], pos piece,
-                    player current_player) {
-  bool isValid = false;
+void selection_pion(pion grille[][WIDTH_TABLE], pos* piece,
+                    move possible_moves[], int nb_moves) {
+  bool is_valid = false;
   int x = 0;
   int y = 0;
-  while (!isValid) {
-    printf("Veuillez sélectionner les coordonnées du pion à bouger ! x y\n");
+  while (!is_valid) {
+    printf(
+        "Veuillez selectionner les coordonnees de la piece a bouger ! x y\n");
+
     scanf("%d %d", &x, &y);
-    if ((x < 0) || (x >= WIDTH_TABLE) || (y < 0) || (y >= HEIGHT_TABLE)) {
-      printf("Valeurs en dehors de la range !!!!\n");
-    } else if (((grille[y][x] == PION_BLACK &&
-                 current_player != PLAYER_BLACK)) ||
-               ((grille[y][x] == PION_WHITE &&
-                 current_player != PLAYER_WHITE))) {
-      printf("Ceci n'est pas votre pion !\n");
-    } else if (grille[y][x] == PION_EMPTY) {
-      printf("Cet endroit est vide ! \n");
-    } else {
-      isValid = true;
-      printf("valeur du pion %d", grille[y][x]);
+
+    for (int moves_index = 0; moves_index < nb_moves; moves_index++) {
+      *piece = possible_moves[moves_index].piece;
+      if ((piece->y == y) && (piece->x == x)) {
+        is_valid = true;
+      }
     }
   }
 }
 
-void selection_move(pion grille[][WIDTH_TABLE], pos piece, pos dest,
-                    player current_player) {
-  bool isValid = false;
+void selection_move(pion grille[][WIDTH_TABLE], pos piece, pos* dest,
+                    move possible_moves[], int nb_moves) {
+  bool is_valid = false;
   int x = 0;
   int y = 0;
-  while (!isValid) {
-    printf("Veuillez sélectionner les coordonnées du pion à bouger ! x y\n");
+  pos temp_piece;
+  while (!is_valid) {
+    printf("Veuillez selectionner les coordonnees de la destination ! x y\n");
     scanf("%d %d", &x, &y);
-    if ((x < 0) || (x >= WIDTH_TABLE) || (y < 0) || (y >= HEIGHT_TABLE)) {
-      printf("Valeurs en dehors de la range !!!!\n");
-    } else if (((grille[y][x] == PION_BLACK &&
-                 current_player != PLAYER_BLACK)) ||
-               ((grille[y][x] == PION_WHITE &&
-                 current_player != PLAYER_WHITE))) {
-      printf("Ceci n'est pas votre pion !\n");
-    } else if (grille[y][x] == PION_EMPTY) {
-      printf("Cet endroit est vide ! \n");
-    } else {
-      isValid = true;
-      printf("valeur du pion %d", grille[y][x]);
+    for (int moves_index = 0; moves_index < nb_moves; moves_index++) {
+      *dest = possible_moves[moves_index].destination;
+      temp_piece = possible_moves[moves_index].piece;
+      printf("la piece x = %d y = %d peut elle aller en x=%d y=%d ?\n", piece.x,
+             piece.y, x, y);
+      printf("celle ci x = %d y = %d peut aller en x = %d y = %d\n",
+             temp_piece.x, temp_piece.y, dest->x, dest->y);
+      if (((dest->y == y) && (dest->x == x)) &&
+          ((temp_piece.y == piece.y) && (temp_piece.x == temp_piece.x))) {
+        is_valid = true;
+      }
     }
   }
 }
-
 bool is_his_piece(pion grille[][WIDTH_TABLE], player current_player,
                   pos piece) {
   if (grille[piece.y][piece.x] == PION_EMPTY) {
@@ -119,26 +139,45 @@ bool is_ennemy_piece(pion grille[][WIDTH_TABLE], player current_player,
   }
   return false;
 }
+void do_move(pion grille[][WIDTH_TABLE], move move_played) {
+  grille[move_played.destination.y][move_played.destination.x] =
+      grille[move_played.piece.y][move_played.piece.x];
+  grille[move_played.piece.y][move_played.piece.x] = PION_EMPTY;
+  int relative_dist = move_played.piece.y - move_played.destination.y;
+  if (relative_dist * relative_dist > 1) {
+    pos ennemy;
+    ennemy.y = (move_played.destination.y + move_played.piece.y) / 2;
+    ennemy.x = (move_played.destination.x + move_played.piece.x) / 2;
+    grille[ennemy.y][ennemy.x] = PION_EMPTY;
+  }
+}
 
-void player_turn(pion grille[][WIDTH_TABLE], player current_player) {}
+void player_turn(pion grille[][WIDTH_TABLE], player current_player) {
+  move possible_moves[40];
+  int nb_moves = 0;
+  get_possible_moves(grille, current_player, possible_moves, &nb_moves);
+  move player_move;
+  ask_joueur_move(grille, &player_move, possible_moves, nb_moves);
+  do_move(grille, player_move);
+}
 
 void check_move_piece(pion grille[][WIDTH_TABLE], player current_player,
                       pos pion, pos dest, move possible_moves[], int* nbMoves) {
-  if (grille[pion.y][pion.x] != PION_EMPTY) {
+  if (grille[dest.y][dest.x] != PION_EMPTY) {
     return;
   }
   move temp;
   temp.destination = dest;
   temp.piece = pion;
   possible_moves[*nbMoves] = temp;
-  nbMoves += 1;
+  *nbMoves += 1;
 }
 
 void check_moves_piece(pion grille[][WIDTH_TABLE], player current_player,
                        pos pion, move possible_moves[], int* nbMoves) {
   pos dest;
   int devant = +1;
-  if (current_player == PLAYER_WHITE) {
+  if (current_player == PLAYER_BLACK) {
     devant = -1;
   }
   if ((pion.y + devant > 0) && (pion.y + devant < HEIGHT_TABLE)) {
@@ -163,14 +202,22 @@ void check_is_eating_move(pion grille[][WIDTH_TABLE], player current_player,
   pos dest;
   dest.y = 2 * possible_ennemy.y - pion.y;
   dest.x = 2 * possible_ennemy.x - pion.x;
+  if ((dest.y < 0) || (dest.y >= HEIGHT_TABLE) || (dest.x < 0) ||
+      (dest.x >= WIDTH_TABLE)) {
+    return;
+  }
   if ((is_ennemy_piece(grille, current_player, possible_ennemy)) &&
       (grille[dest.y][dest.x] == PION_EMPTY)) {
+    if (*can_eat == false) {
+      *can_eat = true;
+      *nbMoves = 0;
+    }
     move eat_move;
     eat_move.piece = pion;
     eat_move.destination = dest;
     possible_moves[*nbMoves] = eat_move;
     *nbMoves += 1;
-    *can_eat = true;
+    printf("je peux manger !\n");
   }
 }
 void check_eat_moves_piece(pion grille[][WIDTH_TABLE], player current_player,
@@ -184,7 +231,7 @@ void check_eat_moves_piece(pion grille[][WIDTH_TABLE], player current_player,
   possible_ennemy.x = pion.x + 1;
   check_is_eating_move(grille, current_player, pion, possible_ennemy,
                        possible_moves, nbMoves, can_eat);
-  possible_ennemy.x = pion.y - 1;
+  possible_ennemy.y = pion.y + 1;
   check_is_eating_move(grille, current_player, pion, possible_ennemy,
                        possible_moves, nbMoves, can_eat);
   possible_ennemy.x = pion.x - 1;
--- lib/dames.h
+++ lib/dames.h
@@ -20,23 +20,24 @@ typedef enum Pion { PION_EMPTY = 0, PION_WHITE, PION_BLACK } pion;
 typedef enum Player { PLAYER_WHITE = 0, PLAYER_BLACK } player;
 
 void init_table(pion grille[][WIDTH_TABLE]);
-
+void game();
 void print_board(pion grille[][WIDTH_TABLE]);
 void print_moves(pion grille[][WIDTH_TABLE], player current_player);
 void print_combos(pion grille[][WIDTH_TABLE], player current_player);
 void print_win();
-
+void player_turn(pion grille[][WIDTH_TABLE], player current_player);
 void ask_joueur(pion grille[][WIDTH_TABLE], pos piece, pos dest,
                 player current_player);
-void selection_pion(pion grille[][WIDTH_TABLE], pos piece,
-                    player current_player);
-void selection_move(pion grille[][WIDTH_TABLE], pos piece, pos dest,
-                    player current_player);
-
+void selection_pion(pion grille[][WIDTH_TABLE], pos* piece,
+                    move possible_moves[], int nb_moves);
+void selection_move(pion grille[][WIDTH_TABLE], pos piece, pos* dest,
+                    move possible_moves[], int nb_moves);
 int compte_piece_equipe(pion grille[][WIDTH_TABLE], player player_counted);
 void one_turn(pion grille[][WIDTH_TABLE]);
 void combo(pion grille[][WIDTH_TABLE], pos movedPion);
 bool is_coup_valide(pion grille[][WIDTH_TABLE], pos piece, pos dest);
 bool is_over(pion grille[][WIDTH_TABLE]);
 void move_pion(pion grille[][WIDTH_TABLE], pos piece, pos dest);
+void get_possible_moves(pion grille[][WIDTH_TABLE], player current_player,
+                        move possible_moves[], int* nbMove);
 #endif
